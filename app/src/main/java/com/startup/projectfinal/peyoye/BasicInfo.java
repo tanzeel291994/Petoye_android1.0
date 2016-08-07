d04947b76bf2bd71f3757b7ba0d0b30cdce6a58d
kirtikarande <2211keerti@gmail.com>
2016-08-07 11:31:08 +0530

    Basic profile Done.

--- app/src/main/java/com/startup/projectfinal/peyoye/BasicInfo.java
+++ app/src/main/java/com/startup/projectfinal/peyoye/BasicInfo.java
@@ -1,15 +1,105 @@
 package com.startup.projectfinal.peyoye;
 
 import android.app.Activity;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import static android.support.v7.appcompat.R.styleable.AlertDialog;
 
 public class BasicInfo extends Activity {
+
+    Spinner spinner_user_category,spinner_pet_category,spinner_breed;
+    String username, user_category, pet_category, breed, alertmsg="";
+    boolean displayerror=false,user_category_set=true,pet_category_set=true,breed_set=true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_basic_info);
 
+        spinner_user_category = (Spinner) findViewById(R.id.spinner_user_category);
+        spinner_pet_category = (Spinner) findViewById(R.id.spinner_pet_category);
+        spinner_breed = (Spinner) findViewById(R.id.spinner_breed);
+
+        spinner_user_category.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                user_category=parent.getItemAtPosition(position).toString();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {}
+        });
+
+        spinner_pet_category.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                pet_category=parent.getItemAtPosition(position).toString();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {}
+        });
+
+        spinner_breed.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                breed=parent.getItemAtPosition(position).toString();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {}
+        });
     }
+
+    public void onClickDoneButton(View view)
+    {
+        alertmsg="";
+        username=((EditText)findViewById(R.id.username)).getText().toString();
+        // push the strings username , user_category, pet_category, breed to database.
+
+        Toast.makeText(this,"Username : "+username,Toast.LENGTH_LONG).show();
+        Toast.makeText(this,"User Category : "+user_category,Toast.LENGTH_LONG).show();
+        Toast.makeText(this," Pet Category : "+pet_category,Toast.LENGTH_LONG).show();
+        Toast.makeText(this," Breed : "+breed,Toast.LENGTH_LONG).show();
+
+        if(username.equals("")|| pet_category.equals("-- Select --") || breed.equals("-- Select --") || user_category.equals("-- Select --"))
+            displayerror=true;
+        else displayerror=false;
+        if(username.equals("")) alertmsg+="Please enter Username. ";
+        if(user_category.equals("-- Select --")) alertmsg+="Please select User category. ";
+        if(pet_category.equals("-- Select --")) alertmsg+="Please select Pet category. ";
+        if(breed.equals("-- Select --")) alertmsg+="Please select Breed. ";
+
+        if(displayerror)    // if information is filled in properly
+        {
+            AlertDialog alertDialog = new AlertDialog.Builder(this).create();
+            alertDialog.setTitle("Alert");
+            alertDialog.setMessage(alertmsg);
+            alertDialog.setButton(DialogInterface.BUTTON_NEUTRAL,"OK", new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog, int which) {
+                    dialog.dismiss();
+                }
+            });
+            alertDialog.show();
+        }
+        else
+        {
+            Intent i = new Intent(this, MainActivity.class);
+            i.putExtra("username",username );i.putExtra("user_category",user_category );
+            i.putExtra("pet_category",pet_category );i.putExtra("breed",breed );
+            startActivity(i);
+        }
+    }
+
 }
