949f10973129f8158d776c1aad6ca0665b1ab6c0
kirtikarande <2211keerti@gmail.com>
2016-08-12 03:58:26 +0530

    messages done notifications done inbox done

--- app/src/main/java/com/startup/projectfinal/peyoye/MainActivity.java
+++ app/src/main/java/com/startup/projectfinal/peyoye/MainActivity.java
@@ -8,6 +8,8 @@ package com.startup.projectfinal.peyoye;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -18,7 +20,21 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
+
+    public  void onClickHomeButton(View view) {}
+
+    public void onClickAddFriendsButton(View view) {}
+
+    public void onClickPlusButton(View view) {}
+
+    public void onClickNotificationButton(View view) {
+        Intent i=new Intent(this, LetterboxScreen.class);
+        startActivity(i);
+    }
+
+    public void onClickMyProfileButton(View view) {}
 
     public void GotoCommentsPage(View view)
     {
